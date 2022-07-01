301dbcc96d4aa63326cf1f394b4070e365373ae3
uptodevo <steannu0405@gmail.com>
2022-07-01 14:42:08 +0100

    first commit

---
+++ products.js
@@ -0,0 +1,27 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  123: {
+    name : "Olives",
+    desc : "tomato sauce, olives and mozzarella.",
+    img : "olives.png",
+    price : 12.00
+  },
+  124: {
+    name : "Margherita",
+    desc : "tomato sauce and mozzarella.",
+    img : "margherita.jpg",
+    price : 9.00
+  },
+  125: {
+    name : "Vegetarian",
+    desc : "tomato sauce, peperoni and mozzarella.",
+    img : "peperoni.jpg",
+    price : 10.00
+  },
+  126: {
+    name : "Salami",
+    desc : "tomato sauce, salami and mozzarella.",
+    img : "salami.jpg",
+    price : 11.00
+  }
+};
